19f1639d84a473376335ed0fb256c9784ea2e95e
Angelin4444 <angelinkmathew@gmail.com>
2024-10-17 14:50:59 +0400

    Codingiscool.py
    Coding is cool.

---
+++ Exercise 01/Cooding is cool.py
@@ -0,0 +1,10 @@
+#Add the first word to the variable word1
+word1 = "coding"
+
+#Add the first word to the variable word2
+word2 = "is"
+
+#Add the first word to the variable word3
+word3 = "cool"
+c = word1 + " " + word2 + " " + word3
+print(c)
