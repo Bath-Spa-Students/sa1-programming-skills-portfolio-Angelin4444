a8ecbf399249d4ab2a9ee1bcaab1726c5bbf709c
Angelin4444 <angelinkmathew@gmail.com>
2024-11-21 14:10:35 +0400

    Hello

---
+++ Exercise 09/Hello.py
@@ -0,0 +1,12 @@
+#Fill in the blanks in the code below so that the function hello() prints "Hello" to the console.
+
+# Fill in this blank to print "Hello" to the console
+def hello(): # hello() is the function name where we will have to print what we want to.
+    print("Hello")# so bellow the hello function we will print what we want here we want it to be 'Hello' which will print hello.
+
+# Fill in this blank to call the hello() function
+def main():# main is also a function name . it will be the place where the main part of the the program will be. 
+    hello()# here the main part will be 'Hello' so wrote the hello function from above.  
+
+if __name__ == "__main__":# this is a line used to check if the code is run directly or not.
+    main()# if its run directly we will write what to run directly. here we want 'Hello' which is the main function of the program.
