cebb22d722a7e210c5fca87d09720ceddffaa032
Angelin4444 <angelinkmathew@gmail.com>
2024-11-21 14:26:58 +0400

    Poem

---
+++ class room activity/Poem.py
@@ -0,0 +1,28 @@
+#Poem No.1
+#By Robert Frost
+#Fire and Ice
+print("""Some say the world will end in fire,
+Some say in ice.
+From what I've tasted of desire
+I hold with those who favor fire.
+But if it had to perish twice,
+I think I know enough of hate
+To say that for destruction ice
+Is also great 
+And would suffice """)
+#Poem No.2
+#By Robert Frost
+#Dust of Snow
+print("""The way a crowshook down on me
+The dust of snow
+From a hemlock tree
+Has given my heart
+Achange of mood
+And saved some part of a day I had rued.""")
+#Poem No.3
+#By Robert Frost
+#Devotion
+print("""The heart can think of no devotion
+Greater than being shore to the ocean--
+Holding the curve of one position,
+Counting an endless repetition.""")
