0c5134cce8c6579a1d334653dd4d8c0e3235f6a1
Angelin4444 <angelinkmathew@gmail.com>
2024-11-21 14:25:50 +0400

    dictionaries

---
+++ class room activity/dictionaries.py
@@ -0,0 +1,21 @@
+# Creation of  dictionary 
+Numbers = {1,2,5,7,4,9}
+print(Numbers) # curly brackets let the numbers to tell that this is dictionary.
+
+# Check the type of dictionary
+
+Numbers = {1,2,5,7,4,9}
+print(Numbers, type(Numbers))
+
+# To add something in dictionary 
+example2 = {'Name' : 'Angelin'  , 'Number' : '4444' ,'Place' : 'India'}
+print (example2)
+
+
+# To add some value in dictionary 
+
+Information= { 'Name' : 'Angelin' , 'Roll No ' :  '4444' , 'Fathers name ': 'Mathew K M' } 
+print(Information,type(Information))
+ # Name , roll no are keys , Angelin and 4444 are values
+
+
