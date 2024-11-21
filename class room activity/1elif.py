f77fdf82e83c645865ea125e510ef69dc6990fca
Angelin4444 <angelinkmathew@gmail.com>
2024-11-21 14:25:08 +0400

    1elif

---
+++ class room activity/1elif.py
@@ -0,0 +1,12 @@
+Score = int (input("enter the score :"))
+if Score >= 90:
+      print("Your grade is A")
+elif Score >= 80:
+      print("Your grade is B")
+elif Score >= 70:
+      print("Your grade is C")
+elif Score >= 60:
+      print("Your grade is D")
+else:
+      print("Your grade is F") 
+
