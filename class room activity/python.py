feb87df549954d5bbdb746ef1960ae23be7e00fe
Angelin4444 <angelinkmathew@gmail.com>
2024-11-21 14:27:06 +0400

    python

---
+++ class room activity/python.py
@@ -0,0 +1,15 @@
+print("Welcome to Code Lab 1")
+print("Self Introduction")
+print("""Hello I am Angelin K Mathew""")
+print("I was born in India but bought up in UAE")
+print("I have finnished my gradution in SIS")
+print("SIS stands for Sharjah Indian School")
+print("""Currently I am doing my Bachelors in Creative Coomputing 
+  from the University of BathSpa UK""")
+print("""Even though its original campus is in UK 
+  I am studying in the RAK campus""")
+print("""I am a music enthusiast,
+    I love singing and dance""")
+print("I am really good in making food, hence making me a good cook")
+print("I am a sweet amd imtroverted girl")
+print("Both my do work")
