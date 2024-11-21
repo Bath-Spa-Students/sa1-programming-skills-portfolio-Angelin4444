b4b1a70ae13b3b8df35f2d744cea681246fade01
Angelin4444 <angelinkmathew@gmail.com>
2024-11-21 14:26:15 +0400

    Lists

---
+++ class room activity/Lists.py
@@ -0,0 +1,28 @@
+#built in methods - index
+
+list = [7,8,3,4,9,5]
+print (list.index(4))  # Identifying the index of number 4
+
+#built in methods - sort
+
+list1 = [5,6,3,9,10,4]
+list1.sort()
+print(list1)     
+
+
+#built in methods - reverse
+
+list2 = [5,6,3,9,10,4]
+list2.reverse()
+print(list1)
+
+#built in methods - remove
+
+list3 = [7,8,3,4,9,5]
+list3.remove(6)
+print(list3)
+
+#min max number
+print (max(list3))
+print (min (list3))
+
