1d0d91aab632172c9fef2a59784c809524928e2e
Angelin4444 <angelinkmathew@gmail.com>
2024-11-21 14:26:22 +0400

    Loops

---
+++ class room activity/Loops.py
@@ -0,0 +1,22 @@
+# With the while loop we can execute a set of statements as long as a condition is true.
+# Print Apple as long as Apple is less than 10:
+
+Apple = 1
+while Apple < 10:
+  print(Apple)
+  Apple += 1  # Apple =Apple+1  
+
+
+  count = 1
+while count <= 5:
+    print("Count is:", count)
+    count += 1
+
+#Infinite Loop With Break Statement 
+
+Orange = 1
+while Orange < 6:
+  print(Orange)
+  if Orange == 3:
+    break
+  Orange += 1   # Orange=Orange+1  Both are same 
