4b43e37f9550799e3818ecdb3bcfaec6508cc9ae
Angelin4444 <angelinkmathew@gmail.com>
2024-11-22 23:24:05 +0400

    List -Names

---
+++ Z_Bonus Exercise/Bonus4.py
@@ -0,0 +1,22 @@
+# 4) List -Names
+
+#Store the names of a few of your friends in a list called names.
+#Print each person’s name by accessing each element in the list, one at a time.
+
+#first creating a list of my friends under the variable name "name"
+names = ["Aneesa", "Minnu", "Melissa", "Hana", "Lois", "Akshaya", "Anuksha", "Aleena", "Gursharan"]
+
+print(names[0])
+print(names[1])
+print(names[2])
+print(names[3])
+print(names[4])
+print(names[5])
+print(names[7])
+print(names[8])
+#printing each persons name by indexing them(always the first string in a list always starts with the no. 0)
+
+
+
+
+
