cdf512e25fc615f1527d9930c898e23d9683b2ea
Angelin4444 <angelinkmathew@gmail.com>
2024-11-23 00:03:18 +0400

    Coding is Cool

--- Exercise 01/Cooding is cool.py
+++ Exercise 01/Cooding is cool.py
@@ -1,10 +1,10 @@
 #Adding the first word to the variable word1
 word1 = "coding"
 
-#Adding the first word to the variable word2
+#Adding the second word to the variable word2
 word2 = "is"
 
-#Adding the first word to the variable word3
+#Adding the third word to the variable word3
 word3 = "cool"
 #Adding all the three variables to make a sentence
 c = word1 + " " + word2 + " " + word3
