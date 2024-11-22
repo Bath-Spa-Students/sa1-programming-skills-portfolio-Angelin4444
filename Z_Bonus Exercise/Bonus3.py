aa3c5bdee36f49190e72268aecee145bc40a3265
Angelin4444 <angelinkmathew@gmail.com>
2024-11-22 23:23:58 +0400

    Variables (Favorite Number)

---
+++ Z_Bonus Exercise/Bonus3.py
@@ -0,0 +1,10 @@
+# 3) Variables (Favorite Number):
+
+#Use a variable to represent your favorite number. 
+#Then, using that variable, create a message that reveals your favorite number. Print that message.
+
+Fav_Num = "4" #(writing my favorite number.)
+
+message = "My favorite no. is" + " " + str(Fav_Num) #(writing a message  and added my fav no. with it)(str is used for making an integer into a string.)
+
+print(message) #(output is made)
