7a6b40356e27b19d9ed4c2540ad9c393f717ea56
Angelin4444 <angelinkmathew@gmail.com>
2024-11-22 23:23:17 +0400

    Strings Concatenation

---
+++ Z_Bonus Exercise/Bonus1.py
@@ -0,0 +1,14 @@
+# 1) Strings Concatenation:
+
+#Write three strings in different variables and print the output as one string.
+
+#Creating 3 different strings with different variable names.
+Concantenation1 = "My favorite"
+Concantenation2 = "book is"
+Concantenation3 = "'Dork Diaries'."
+#concantenation is the common variable name i took and the 3 string are written above.
+One_String = Concantenation1 + " " + Concantenation2 + " " + Concantenation3 
+#(the above line is wrote to sum up the the 3 strings to be in one line.
+# the ("")is used to make spaces between the the 3 different strings.)
+print(One_String)
+#output is made in the above print line.
