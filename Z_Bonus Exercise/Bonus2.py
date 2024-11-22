a3f6445194d4f963f184f6b50d47541a7a846bef
Angelin4444 <angelinkmathew@gmail.com>
2024-11-22 23:23:44 +0400

    Variables

---
+++ Z_Bonus Exercise/Bonus2.py
@@ -0,0 +1,15 @@
+# 2) Variables:
+
+#Assign a message to a variable, and print that message. 
+# Then change the value of the variable to a new message, and print the new message.
+
+#first writing a message to the variable.
+Fruits = "Mango"#(this is the first message i am assigning to the variable)
+print(Fruits)#(printing the first variable)
+
+Fruits = "Strawberry"#(this is the second message i am assigning to the variable)
+print(Fruits)#(printing the second variable)
+
+#now both the new and the old message will be shown when run
+
+
